c2d6c7e86993a2a30e282aa89bf30b0ca938bdb5
Connor <Connorh223@gmail.com>
2021-08-07 15:40:00 -0400

    add main.py

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import random
 def get_random_word(difficulty: int): 
     f = open("words.txt", "r")
     txt = f.read()
-    print(f.read())
     # print(txt)
-    random_number = random.randint(0, 100)
     txt_array = txt.split("\n")
-    print(txt_array[random_number])
+    filtered_list = [x for x in txt_array if len(x) >= 4 and len(x) <= 6]
+    random_number = random.randint(0, len(filtered_list))
+    return(filtered_list[random_number])
 
 
-get_random_word(0)
+my_random_word = get_random_word(0)
+print(my_random_word)
 # print(5)
